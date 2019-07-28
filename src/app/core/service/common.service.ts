3f10f3ef30f132a95d556013315ceedc72420100
Deepu Vijayan <deepu.vijayan2007@gmail.com>
2019-07-28 16:43:35 +0530

    alert moved to common services

--- src/app/core/service/common.service.ts
+++ src/app/core/service/common.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { DialogComponent } from '../../modules/sharedModule/components/dialog/dialog.component';
+import { MatDialog, MatDialogRef } from '@angular/material';
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   saveItem( key , data){
     sessionStorage.setItem(key, JSON.stringify(data));
+  }
+
+  showAlertDialog(data){
+    let dialogBox = this.dialog.open(DialogComponent, {
+          //hasBackdrop: false,
+          data
+        });
+    dialogBox.afterClosed().subscribe(result => {
+      dialogBox.close();
+    });
   }
 
 
--- src/app/modules/authenticate/login/login.component.ts
+++ src/app/modules/authenticate/login/login.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonService } from '../../../core/service/common.service';
 import { WebapiService } from '../../../core/http/webapi.service';
 import { SocialApiService } from '../../../core/service/socialapi.service';
-import { DialogComponent } from '../../sharedModule/components/dialog/dialog.component';
 
-import { MatDialog, MatDialogRef } from '@angular/material';
+
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -14,7 +13,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor(private router: Router, private dialog: MatDialog, private commonService: CommonService, private webapiService: WebapiService, private socialApiService: SocialApiService) { }
+  constructor(private router: Router, private commonService: CommonService, private webapiService: WebapiService, private socialApiService: SocialApiService) { }
   public auth2: any;
   settingsObs: any;
   settingsData:any;
@@ -34,25 +33,18 @@ export class LoginComponent implements OnInit, OnDestroy {
     switch(type){
       case "facebook":{
         this.socialApiService.signInWithFaceBook();
+        break;
       }
       case "google":{
-        let dialogBox = this.dialog.open(DialogComponent, {
-          //hasBackdrop: false,
-          data: {
-            message: 'Error Message'
-          }
+        this.commonService.showAlertDialog({
+          message: 'Error Message'
         });
-        dialogBox.afterClosed().subscribe(result => {
-          // NOTE: The result can also be nothing if the user presses the `esc` key or clicks outside the dialog
-            console.log(result);
-            if(result =='ok'){
-              dialogBox.close();
-            }
-        })
         //this.socialApiService.attachSignin(event.currentTarget);
+        break;
       }
       case "home":{
         this.router.navigate(['home']);
+        break;
       }
     }
     //this.router.navigate(['../authenticate/invite/all'])
